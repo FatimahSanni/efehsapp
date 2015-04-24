83a87884a314bbde7eea85482dc44a9c32cc3f96
Fatimah Sanni <fatimah.sanni@gmail.com>
2015-04-24 04:17:28 +0100

    Added consultant's master record

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424030938) do
+ActiveRecord::Schema.define(version: 20150424031442) do
+
+  create_table "consultants", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string   "mrn"
